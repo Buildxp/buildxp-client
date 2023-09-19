37c6008cc2c516cdf9d96fc75c64c7b6a6d5471c
arrryan05 <mishra2001aryan@gmail.com>
2023-09-20 00:15:04 +0530

    footer

---
+++ rehome/src/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Footer.scss";
+
+export default function Footer() {
+  return (
+    <div className="footer-main">
+      <div className="top">
+        <div className="top-left">
+          <div className="internal-div-1"></div>
+          <div className="internal-div-2"></div>
+        </div>
+        <div className="top-right">
+          <div className="internal-div-3"></div>
+          <div className="internal-div-4"></div>
+        </div>
+      </div>
+      <div className="bottom"></div>
+    </div>
+  );
+}
--- rehome/src/pages/Home/Home.jsx
+++ rehome/src/pages/Home/Home.jsx
@@ -1,7 +1,11 @@
-import './Home.scss';
+import "./Home.scss";
+import Footer from "../../components/Footer/Footer";
 
 export default function Home() {
-    return(
-        <h1>Home page</h1>
-    )
-};
+  return (
+    <div className="main">
+      <h1>Home page</h1>
+      <Footer />
+    </div>
+  );
+}
