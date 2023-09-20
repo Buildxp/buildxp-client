955a17443d81da157b8cd0753901ee9cd5adbd4e
arrryan05 <mishra2001aryan@gmail.com>
2023-09-20 11:44:30 +0530

    footer

--- rehome/src/pages/Home/Home.jsx
+++ rehome/src/pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import Img1 from "./asset/left.png";
 import Img2 from "./asset/card1.png";
 import ArrowCircleRightOutlinedIcon from "@mui/icons-material/ArrowCircleRightOutlined";
+import Footer from "../../components/Footer/Footer";
 
 const Carddata = [
   {
@@ -29,39 +30,42 @@ const Carddata = [
 
 export default function Home() {
   return (
-    <div id="main">
-      <div className="innerdiv">
-        <div className="leftinnerdiv">
-          <div className="heading">
-            <h1>
-              Let's Reunite every <span>Missing</span> Child{" "}
-            </h1>
-            <p>of the country with their families</p>
+    <div className="root">
+      <div id="main">
+        <div className="innerdiv">
+          <div className="leftinnerdiv">
+            <div className="heading">
+              <h1>
+                Let's Reunite every <span>Missing</span> Child{" "}
+              </h1>
+              <p>of the country with their families</p>
+            </div>
+            <div className="innerbtn">
+              <button className="bt1">Report Missing Child</button>
+              <button className="bt2">Found Missing Child</button>
+            </div>
           </div>
-          <div className="innerbtn">
-            <button className="bt1">Report Missing Child</button>
-            <button className="bt2">Found Missing Child</button>
+          <div className="rightinnerdiv">
+            <img src={Img1} />
           </div>
         </div>
-        <div className="rightinnerdiv">
-          <img src={Img1} />
+
+        {/* card div */}
+
+        <div id="card-section">
+          {Carddata.map((data, i) => (
+            <div className="card">
+              <img src={data.main} />
+              <h3>{data.head}</h3>
+              <p>{data.para}</p>
+              <span className="icon">
+                <ArrowCircleRightOutlinedIcon />
+              </span>
+            </div>
+          ))}
         </div>
       </div>
-
-      {/* card div */}
-
-      <div id="card-section">
-        {Carddata.map((data, i) => (
-          <div className="card">
-            <img src={data.main} />
-            <h3>{data.head}</h3>
-            <p>{data.para}</p>
-            <span className="icon">
-              <ArrowCircleRightOutlinedIcon />
-            </span>
-          </div>
-        ))}
-      </div>
+      <Footer />
     </div>
   );
 }
